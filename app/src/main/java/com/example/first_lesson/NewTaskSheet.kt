6ffc3158511940746d11d86670b5672c3e4fdaf4
Moldir <moldir.andirbekova@mail.ru>
2024-02-23 11:45:41 +0600

    Added onClickListener to saveTaskButton

--- app/src/main/java/com/example/first_lesson/NewTaskSheet.kt
+++ app/src/main/java/com/example/first_lesson/NewTaskSheet.kt
@@ -5,20 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
-
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.example.first_lesson.databinding.FragmentNewTaskSheetBinding
 
 class NewTaskSheet : BottomSheetDialogFragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private lateinit var binding: FragmentNewTaskSheetBinding
+    private lateinit var taskViewModel: TaskViewModel
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.saveButton.setOnClickListener{
+            saveAction()
+        }
     }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_new_task_sheet, container, false)
+        binding = FragmentNewTaskSheetBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    private fun saveAction() {
+        taskViewModel.name.value = binding.name.text.toString()
+        taskViewModel.desc.value = binding.taskDesc.text.toString()
+        binding.name.setText("")
+        binding.taskDesc.setText("")
+        dismiss()
     }
 
 
