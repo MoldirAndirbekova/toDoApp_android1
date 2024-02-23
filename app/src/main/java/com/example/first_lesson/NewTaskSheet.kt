7bd85c42786f19b0abd4ad3424b4de6594f9386d
Moldir <moldir.andirbekova@mail.ru>
2024-02-23 17:44:14 +0600

    Modified saveButton() method and added RecycleView

--- app/src/main/java/com/example/first_lesson/NewTaskSheet.kt
+++ app/src/main/java/com/example/first_lesson/NewTaskSheet.kt
@@ -2,7 +2,6 @@ package com.example.first_lesson
 
 import android.os.Bundle
 import android.text.Editable
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
